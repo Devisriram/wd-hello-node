f9e95844920172120b3a7e258e0f3d3847173146
root <root@DeviSriram.localdomain>
2022-11-26 15:03:41 +0530

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-const returnHello = () => {
-    console.log("Hello Node.js!");
-}
+const http = require('http');
+const fs = require('fs');
 
-returnHello();
+//const args = require("minimist")(process.argv.slice(2));
+
+const args = require("minimist")(process.argv.slice(1));
+// console.log(args);
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args, () => {
+    console.log(`Your app is being running in the port ${args.port}`);
+  });
+
+
